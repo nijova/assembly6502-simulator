18c0efcbfdb72cce39cfd427975dd7ea54e8d975
nijova <niko.jovan@protonmail.com>
2022-03-03 16:15:27 -0500

    refactor

--- helpers.js
+++ helpers.js
@@ -1,20 +1,20 @@
 const hex = (n, width=2) => n.toString(16).padStart(width, '0');
 
 const initMemory = () => {
-  let maxMemory = 2**8;
-  let width = hex(maxMemory - 1).length;
+  let maxMemory = 16;
   const memory = document.getElementById('memory');
   for (let i=0; i<maxMemory; i++) {
-    let hexI = hex(i);
+    let ii = i * 16;
+    let index = hex(ii, 4);
     let row = document.createElement('tr');
-    row.id = 'row' + hexI;
+    row.id = 'row' + index;
     let rowIndex = document.createElement('td');
-    rowIndex.innerText = hexI;
+    rowIndex.innerText = index;
     row.appendChild(rowIndex);
     for (let j=0; j<16; j++) {
       let hexJ = hex(j);
       let entry = document.createElement('td');
-      entry.id = '' + hexI + hexJ;
+      entry.id = '' + hex(ii) + hexJ;
       entry.innerText = '00';
       row.appendChild(entry);
     }
