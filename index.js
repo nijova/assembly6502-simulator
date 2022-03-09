cc797d82523f73401e302b3814bf929caca59a48
nijova <niko.jovan@protonmail.com>
2022-03-09 15:29:40 -0500

    flags

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const memory = document.getElementById('memory');
 initMemory(memory);
+
 let A, X, Y = 0;
+const FLAGS = {
+  'Negative': false, 
+  'oVerflow':false, 
+  'Break':false, 
+  'Decimal': false,
+  'Interrupt': false,
+  'Zero': false,
+  'Carry': false
+}
+
 reset();
